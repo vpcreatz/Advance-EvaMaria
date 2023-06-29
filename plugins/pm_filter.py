70397509e2234451a154474d19d3ac7483b6923d
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-06-29 16:18:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,15 +65,28 @@ async def next_page(bot, query):
         return
     settings = await get_settings(query.message.chat.id)
     if settings['button']:
-        btn = <a href=https://t.me/{temp.U_NAME}?start={ident}_{file_id}><b></b>{get_size(file.file_size)}</a>\n
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                ),
+            ]
             for file in files
         ]
     else:
-        btn = <a href=https://t.me/{temp.U_NAME}?start={ident}_{file_id}><b></b>{get_size(file.file_size)}</a>\n,
-        
-                <a href=https://t.me/{temp.U_NAME}?start={ident}_{file_id}><b></b>{get_size(file.file_size)}/a>\n
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                ),
+                InlineKeyboardButton(
+                    text=f"{get_size(file.file_size)}",
+                    callback_data=f'files_#{file.file_id}',
+                ),
+            ]
             for file in files
-
+        ]
+        
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
